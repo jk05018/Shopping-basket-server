28b8f3373401cf5a55a9113b54d142253e665af4
SeungHan <jk05018@naver.com>
2022-04-29 23:35:21 +0900

    feat : dataSource 설정 및 database 설계 완료

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,54 @@
+create table products
+(
+    product_id BINARY(16) PRIMARY KEY,
+    product_name VARCHAR(20) NOT NULL,
+    price bigint NOT NULL,
+    remain_quantity int NOT NULL,
+    description VARCHAR(500) DEFAULT NULL,
+    created_at datetime(6) NOT NULL,
+    updated_at datetime(6) DEFAULT NULL
+);
+
+
+create table orders
+(
+    order_id BINARY(16) PRIMARY KEY ,
+    voucher_id BINARY(16) ,
+    email VARCHAR(50) NOT NULL,
+    address VARCHAR(200) NOT NULL,
+    postcode VARCHAR(200) NOT NULL,
+    created_at datetime(6) NOT NULL,
+    updated_at datetime(6) DEFAULT NULL,
+    CONSTRAINT fk_orders_to_vouchers FOREIGN KEY (voucher_id) REFERENCES vouchers (voucher_id)
+);
+
+create table order_items
+(
+    seq bigint NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    order_id BINARY(16) NOT NULL,
+    product_id BINARY(16) NOT NULL,
+    price bigint NOT NULL,
+    quantity int NOT NULL,
+    created_at datetime(6) NOT NULL,
+    updated_at datetime(6) DEFAULT NULL,
+    INDEX (order_id),
+    CONSTRAINT fk_order_items_to_order FOREIGN KEY (order_id) REFERENCES orders(order_id) ON DELETE CASCADE,
+    CONSTRAINT fk_order_items_to_product FOREIGN KEY (product_id) REFERENCES products (product_id)
+);
+
+create table vouchers
+(
+    voucher_id BINARY(16) PRIMARY KEY ,
+    value int NOT NULL,
+    type BINARY(16) NOT NULL,
+    description VARCHAR(200) NOT NULL,
+    created_at datetime(6) NOT NULL,
+    updated_at datetime(6) DEFAULT NULL,
+    CONSTRAINT fk_vouchers_to_voucher_type FOREIGN KEY (type) REFERENCES voucher_type (voucher_type_id)
+);
+
+create table voucher_type
+(
+    voucher_type_id BINARY(16) PRIMARY KEY ,
+    name VARCHAR(20) NOT NULL
+);
