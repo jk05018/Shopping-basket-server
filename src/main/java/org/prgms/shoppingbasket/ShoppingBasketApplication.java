7e6c1df67f3ed734d380e34962390049ef351339
SeungHan <jk05018@naver.com>
2022-04-29 23:11:50 +0900

    build : shopping basket 초기 build

---
+++ src/main/java/org/prgms/shoppingbasket/ShoppingBasketApplication.java
@@ -0,0 +1,13 @@
+package org.prgms.shoppingbasket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingBasketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingBasketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/prgms/shoppingbasket/ShoppingBasketApplicationTests.java
@@ -0,0 +1,13 @@
+package org.prgms.shoppingbasket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingBasketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
