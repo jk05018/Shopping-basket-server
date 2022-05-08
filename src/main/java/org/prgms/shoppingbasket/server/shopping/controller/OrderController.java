25aad7150a0431a9fe0cd1f97c11d5353d1286d6
SeungHan <jk05018@naver.com>
2022-05-08 15:05:36 +0900

    fix : contoller 로직 -> service 로직으로 이동

--- src/main/java/org/prgms/shoppingbasket/server/shopping/controller/OrderController.java
+++ src/main/java/org/prgms/shoppingbasket/server/shopping/controller/OrderController.java
@@ -7,13 +7,14 @@ import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 import org.prgms.shoppingbasket.server.common.exception.ApiException;
-import org.prgms.shoppingbasket.server.common.exception.ApiExceptionEntity;
 import org.prgms.shoppingbasket.server.common.exception.ExceptionEnum;
 import org.prgms.shoppingbasket.server.shopping.dto.OrderCreateDto;
 import org.prgms.shoppingbasket.server.shopping.dto.OrderUpdateDto;
 import org.prgms.shoppingbasket.server.shopping.entity.Order;
 import org.prgms.shoppingbasket.server.shopping.service.OrderService;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -35,41 +36,50 @@ public class OrderController {
 	private final OrderService orderService;
 
 	@GetMapping
-	public List<Order> getOrderList(){
-		return orderService.getAllOrders();
+	public ResponseEntity<List<Order>> getOrderList() {
+		return ResponseEntity
+			.status(HttpStatus.OK)
+			.body(orderService.getAllOrders());
 	}
 
 	@PostMapping
-	public Order createOrder(@Valid @RequestBody OrderCreateDto orderRequest, BindingResult bindingResult) {
+	public ResponseEntity<Order> createOrder(@Valid @RequestBody OrderCreateDto orderRequest,
+		BindingResult bindingResult) {
 
-		if(bindingResult.hasErrors()){
+		if (bindingResult.hasErrors()) {
 			throw new ApiException(ExceptionEnum.BINDING_EXCEPTION.withMessage(errorToMessage(bindingResult)));
 		}
-		if (orderRequest.getVoucherId().size() != 0) {
-			return orderService.createOrder(orderRequest.getVoucherId().get(0), orderRequest.getEmail(),
-				orderRequest.getAddress(), orderRequest.getPostcode(), orderRequest.getOrderItems());
-		}
-		return orderService.createOrder(null, orderRequest.getEmail(), orderRequest.getAddress()
-			, orderRequest.getPostcode(), orderRequest.getOrderItems());
+
+		Order order = orderService.createOrder(orderRequest.getVoucherId().get(0), orderRequest.getEmail(),
+			orderRequest.getAddress(), orderRequest.getPostcode(), orderRequest.getOrderItems());
+
+		return ResponseEntity
+			.status(HttpStatus.CREATED)
+			.body(order);
 	}
 
 	@PostMapping("/{orderId}")
-	public Order updateDeleveryDestination(@PathVariable UUID orderId, @Valid @RequestBody OrderUpdateDto orderUpdateDto,BindingResult bindingResult, HttpServletResponse response) {
+	public ResponseEntity<Order> updateDeleveryDestination(@PathVariable UUID orderId,
+		@Valid @RequestBody OrderUpdateDto orderUpdateDto, BindingResult bindingResult, HttpServletResponse response) {
 
-		if(bindingResult.hasErrors()){
+		if (bindingResult.hasErrors()) {
 			throw new ApiException(ExceptionEnum.BINDING_EXCEPTION.withMessage(errorToMessage(bindingResult)));
 		}
 
-		return orderService.updateDeliveryDestination(orderId, orderUpdateDto.getEmail(), orderUpdateDto.getAddress(),
-			orderUpdateDto.getPostcode());
+		final Order order = orderService.updateDeliveryDestination(orderId, orderUpdateDto.getEmail(),
+			orderUpdateDto.getAddress(), orderUpdateDto.getPostcode());
+
+		return ResponseEntity
+			.status(HttpStatus.ACCEPTED)
+			.body(order);
 	}
 
 	@DeleteMapping("/{orderId}")
-	public void deleteOrder(@PathVariable UUID orderId){
+	public void deleteOrder(@PathVariable UUID orderId) {
 		orderService.deleteOrder(orderId);
 	}
 
-	private String errorToMessage(BindingResult bindingResult){
+	private String errorToMessage(BindingResult bindingResult) {
 		StringBuilder builder = new StringBuilder();
 		bindingResult.getAllErrors().stream()
 			.map(DefaultMessageSourceResolvable::getDefaultMessage)
